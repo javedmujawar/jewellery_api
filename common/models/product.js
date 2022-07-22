80cfd08274baff4123a179ac66b0360ee5572ec2
DESKTOP-QQ0KH46\RASHID <kalawant.abdulrashid@gmail.com>
2022-07-22 19:05:02 +0530

    Work Done.

--- common/models/product.js
+++ common/models/product.js
@@ -1,4 +1,17 @@
 'use strict';
 module.exports = function(Product) {
+      //To get product key value pair list.
+      Product.getListKV = function (cb) {
+		Product.dataSource.connector.query("select id,name from product where isDeleted=0", (err, results) => {
+			cb(err, results);
+		});	
+	};
+
+    Product.remoteMethod('getListKV', {
+        description: 'To get product key value pair list.',
+        http: { path: "", verb: 'GET' },
+        accepts: [],
+        returns: { type: 'array', root: true }
+    });
      
 };
